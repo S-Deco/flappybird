56259121a2ba56fed48e7746d76bf48affb5509e
Sam DeCosta <samdeco14@gmail.com>
2018-12-18 02:00:59 -0800

    Score Label setup

--- flappybird/GameScene.swift
+++ flappybird/GameScene.swift
@@ -9,81 +9,104 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory {
+    static let Wall : UInt32 = 0x1 << 1
+    static let Bird : UInt32 = 0x1 << 2
+    
+}
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bird = SKSpriteNode()
+    var jump = 0
+    var gameStarted = false
+    var scoreLabel = SKLabelNode()
+    var score = 0
+
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        bird = self.childNode(withName: "bird") as! SKSpriteNode
+        scoreLabel = self.childNode(withName:"score") as! SKLabelNode
+        scoreLabel.position.y = self.size.height/4
+        bird.position.y = 0
+        bird.position.x = -self.size.width/3
+        bird.physicsBody?.collisionBitMask = PhysicsCategory.Wall
+        bird.physicsBody?.contactTestBitMask = PhysicsCategory.Wall
+        bird.physicsBody?.categoryBitMask = PhysicsCategory.Bird
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height/2)
+        bird.physicsBody?.affectedByGravity = true
+        bird.physicsBody?.isDynamic = true
+        bird.physicsBody?.mass = 0.1
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func createWall(){
+        let wall = SKNode()
+        let randPoint = Int.random(in: Int(-self.size.height/2 + 10)...Int(self.size.height/2 - 10))
+        let randWidth = Int.random(in: 80...Int(self.size.height/2))
+        let top = SKSpriteNode(imageNamed: "wall")
+        let bottom = SKSpriteNode(imageNamed:"wall")
+        top.size = CGSize(width: 60, height: self.size.height)
+        bottom.size = (CGSize(width: 60, height: self.size.height))
+        
+        top.physicsBody = SKPhysicsBody(rectangleOf: top.size)
+        top.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        top.physicsBody?.collisionBitMask = PhysicsCategory.Bird
+        top.physicsBody?.contactTestBitMask = PhysicsCategory.Bird
+        top.physicsBody?.isDynamic = false
+        top.physicsBody?.affectedByGravity = false
+        
+        bottom.physicsBody = SKPhysicsBody(rectangleOf: bottom.size)
+        bottom.physicsBody?.categoryBitMask = PhysicsCategory.Wall
+        bottom.physicsBody?.collisionBitMask = PhysicsCategory.Bird
+        bottom.physicsBody?.contactTestBitMask = PhysicsCategory.Bird
+        bottom.physicsBody?.isDynamic = false
+        bottom.physicsBody?.affectedByGravity = false
+        
+        
+        
+        top.position = CGPoint(x: self.size.width/2 + 10, y: CGFloat(randPoint + Int(self.size.height/2) + 80))
+        bottom.position = CGPoint(x: self.size.width/2 + 10, y: CGFloat(randPoint - Int(self.size.height/2) - randWidth))
+        
+        wall.addChild(top)
+        wall.addChild(bottom)
+        
+        self.addChild(wall)
+        wall.run(SKAction.moveTo(x: -self.size.width - 80, duration: 2.5))
+        
+
+        
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    func startSpawn(){
+        let wait = SKAction.wait(forDuration: 1.7)
+        let spawn = SKAction.run {
+            self.createWall()
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let delay = SKAction.sequence([wait,spawn])
+        let delayForever = SKAction.repeatForever(delay)
+        
+        self.run(delayForever)
+        
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        bird.physicsBody?.velocity = (CGVector(dx: 0, dy: 0))
+        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: self.size.height/15))
+        if gameStarted == false
+        {
+            gameStarted = true
+            startSpawn()
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
     }
 }
